1cdf81e79ca65ff1acbff2d9aa43fefb3dd40c01
lichang <798602874@qq.com>
2019-09-02 19:58:09 +0800

    更新使用@observale修饰state变量、@action、@computed，@observer修饰React组件为响应式组件

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@
  * @describe:
  * @create: 2019-09-02 11:17
  */
-import {observable, autorun, computed} from "mobx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {observable, autorun, computed, action, configure} from "mobx";
+import {observer} from 'mobx-react';
 
 const value = observable.box(0);
 const number = observable.box(100);
+
+autorun(() => {
+    console.log(value.get());
+});
+
+value.set(1);
+value.set(2);
+number.set(101);
 
 const map = observable.map({key: "value"});
 map.set("key", "new value");
@@ -24,21 +35,92 @@ const person = observable({
 person.firstName = "C.S.";
 console.log(person);
 
-autorun(() => {
-    console.log(value.get());
-});
+class OrderLine {
+    @observable price = 2;
+    @observable amount = 15;
+
+    @computed get total(){
+        return this.price * this.amount;
+    }
+}
+
+let orderLine = new OrderLine();
+//total函数直接调用获取结果
+console.log("price" + orderLine.price + "total" + orderLine.total);
+
+const num = observable.box(10);
+const plus = computed(() => num.get() > 0);
+ autorun(() => {
+     console.log(plus.get());
+ });
+
+ num.set(-19);
+ num.set(-1);
+ num.set(1);
+
+ const price = observable.box(199);
+ const storage = observable.box(15);
+
+ const allPrice = computed(() => price.get() * storage.get());
+ console.log(`计算${allPrice}`);
+
+ configure({enforceActions: "always"});
+ class Store {
+     @observable number = 0;
+     @action add = () => {
+         this.number++;
+     }
+ }
+ const newStore = new Store();
+ newStore.add();
+ console.log(newStore.number);
+
+ //由MyState进行状态管理
+ class MyState {
+     @observable num1 = 0;
+     @observable num2 = 100;
+
+     @action addNum1 = () => {
+         this.num1++;
+     };
+     @action addNum2 = () => {
+         this.num2++;
+     };
+     @computed get total(){
+         return this.num1 + this.num2;
+     }
+ }
+ //实例化的state状态管理作为props传入子组件
+ const newState = new MyState();
+
+ //使用observer修饰无状态组件
+ const AllNum = observer((props) => <div>num1 + num2 = {props.store.total}</div>);
+
+ const Main = observer((props) => (
+     <div>
+         <p>num1 = {props.store.num1}</p>
+         <p>num2 = {props.store.num2}</p>
+         <div>
+             <button onClick={props.store.addNum1}>num1 + 1</button>
+             <button onClick={props.store.addNum2}>num2 + 1</button>
+         </div>
+     </div>
+ ));
+
+ @observer
+ class App extends React.Component{
+     render() {
+         return (
+             <div>
+                 <Main store={newState}/>
+                 <AllNum store={newState}/>
+             </div>
+         );
+     }
+ }
 
 
-value.set(1);
-value.set(2);
-number.set(101);
-
-// class OrderLine {
-//     @observable price = 0;
-//     @observable amount = 1;
-//
-//     @computed get
-//     total(){
-//         return this.price * this.amount;
-//     }
-// }
+ ReactDOM.render(
+     <App />,
+     document.getElementById('root')
+ );
