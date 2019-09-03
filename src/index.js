f0c21b7f731e7594bccee6d93e90a5bb8c0c9663
lichang <798602874@qq.com>
2019-09-03 11:04:55 +0800

    更新使用mobx构建todo应用，开启严格模式及使用inject将组件连接到提供的stores

--- src/index.js
+++ src/index.js
@@ -1,151 +1,110 @@
 /**
  * @file index.js
  * @author: cdlichang1
- * @describe:
- * @create: 2019-09-02 11:17
+ * @describe: react + mobx TodoList应用
+ * @create: 2019-09-03 09:58
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {observable, autorun, computed, action, configure} from "mobx";
-import {inject, observer, Provider} from 'mobx-react';
+import {observable, computed, configure, action} from "mobx";
+import {inject, observer, Provider} from "mobx-react";
 
-const value = observable.box(0);
-const number = observable.box(100);
+//开启严格模式，只有在action中才能修改观测量state的值
+configure({enforceActions: 'always'});
+//定义数据结构
+class TodoData {
+    @observable todos = [
+        {
+            id: 1,
+            title: 'todo #1',
+            finished: false
+        },
+        {
+            id: 2,
+            title: 'todo #2',
+            finished: false
+        },
+        {
+            id: 3,
+            title: 'todo #3',
+            finished: false
+        }
+    ];
 
-autorun(() => {
-    console.log(value.get());
-});
+    @action finishTodo(todo){
+        todo.finished = !todo.finished;
+    }
 
-value.set(1);
-value.set(2);
-number.set(101);
-
-const map = observable.map({key: "value"});
-map.set("key", "new value");
-console.log(map);
-
-const list = observable([1, 2, 4]);
-list[2] = 3;
-console.log(list);
-
-const person = observable({
-    firstName: 'Clive Staples',
-    lastName: 'Lewis'
-});
-person.firstName = "C.S.";
-console.log(person);
-
-class OrderLine {
-    @observable price = 2;
-    @observable amount = 15;
-
-    @computed get total(){
-        return this.price * this.amount;
+    //返回计算未完成的数量
+    @computed get unfinishedTodoCount(){
+        return this.todos.filter(todo => !todo.finished).length;
     }
 }
 
-let orderLine = new OrderLine();
-//total函数直接调用获取结果
-console.log("price" + orderLine.price + "total" + orderLine.total);
+//静态构建
+@inject('todoList')
+@observer
+class TodoListView extends React.Component{
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.todoList.todos.map(todo =>
+                        <TodoView todo={todo} key={todo.id}/>
+                    )}
+                </ul>
+                未完成任务数：{this.props.todoList.unfinishedTodoCount}
+            </div>
+        );
+    }
+}
 
-const num = observable.box(10);
-const plus = computed(() => num.get() > 0);
- autorun(() => {
-     console.log(plus.get());
- });
+@inject('todoList')
+@observer
+class TodoView extends React.Component{
+    render() {
+        let todo = this.props.todo;
+        //注意在onChange中应该是执行一个函数
+        return (
+            <li>
+                <label>
+                    <input type="checkbox"
+                           checked={todo.finished}
+                           onChange={() => {this.props.todoList.finishTodo(todo)}}
+                    />
+                    {todo.title}
+                </label>
+            </li>
+        );
+    }
 
- num.set(-19);
- num.set(-1);
- num.set(1);
+}
 
- const price = observable.box(199);
- const storage = observable.box(15);
+class ToDoApp extends React.Component{
+    render() {
+        return (
+            <div>
+                <TodoListView />
+            </div>
+        );
+    }
+}
 
- const allPrice = computed(() => price.get() * storage.get());
- console.log(`计算${allPrice}`);
+//初始化数据结构state管理
+const todoList = new TodoData();
 
- configure({enforceActions: "always"});
- class Store {
-     @observable number = 0;
-     @action add = () => {
-         this.number++;
-     }
- }
- const newStore = new Store();
- newStore.add();
- console.log(newStore.number);
+//在Provider中写入todoList作为store，然后在各子组件中通过inject引入
+class App extends React.Component{
+    render() {
+        return (
+            <Provider todoList={todoList}>
+                <ToDoApp />
+            </Provider>
+        );
+    }
+}
 
- //由MyState进行状态管理
- class MyState {
-     @observable num1 = 0;
-     @observable num2 = 100;
-
-     @action addNum1 = () => {
-         this.num1++;
-     };
-     @action addNum2 = () => {
-         this.num2++;
-     };
-     @computed get total(){
-         return this.num1 + this.num2;
-     }
- }
- //实例化的state状态管理作为props传入子组件
- const newState = new MyState();
-
- //使用observer修饰无状态组件
- const AllNum = observer((props) => <div>num1 + num2 = {props.store.total}</div>);
-
- const Main = inject('persons')(observer((props) => (
-
-     <div>
-         <p>num1 = {props.store.num1}</p>
-         <p>num2 = {props.store.num2}</p>
-         <div>
-             <button onClick={props.store.addNum1}>num1 + 1</button>
-             <button onClick={props.store.addNum2}>num2 + 1</button>
-         </div>
-         <ul>
-             {props.persons.value.length && props.persons.value.map((item, index) => <li key={index}>{item.name}: {item.age}</li>)}
-         </ul>
-     </div>
- )));
-
- const persons = observable.box([{
-     name: 'li',
-     age: '24'
- }, {
-     name: 'sun',
-     age: '26'
- }]);
-
- console.log(persons);
-
- @observer
- class MyPerson extends React.Component{
-     render() {
-         return (
-             <div>
-                 <Main store={newState}/>
-                 <AllNum store={newState}/>
-             </div>
-         );
-     }
- }
-
- @observer
- class App extends React.Component{
-     render() {
-         return (
-             <Provider persons={persons}>
-                 <MyPerson />
-             </Provider>
-         );
-     }
- }
-
-
- ReactDOM.render(
-     <App />,
-     document.getElementById('root')
- );
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
---
+++ src/index_mobx.js
@@ -0,0 +1,151 @@
+/**
+ * @file index.js
+ * @author: cdlichang1
+ * @describe:
+ * @create: 2019-09-02 11:17
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {observable, autorun, computed, action, configure} from "mobx";
+import {inject, observer, Provider} from 'mobx-react';
+
+const value = observable.box(0);
+const number = observable.box(100);
+
+autorun(() => {
+    console.log(value.get());
+});
+
+value.set(1);
+value.set(2);
+number.set(101);
+
+const map = observable.map({key: "value"});
+map.set("key", "new value");
+console.log(map);
+
+const list = observable([1, 2, 4]);
+list[2] = 3;
+console.log(list);
+
+const person = observable({
+    firstName: 'Clive Staples',
+    lastName: 'Lewis'
+});
+person.firstName = "C.S.";
+console.log(person);
+
+class OrderLine {
+    @observable price = 2;
+    @observable amount = 15;
+
+    @computed get total(){
+        return this.price * this.amount;
+    }
+}
+
+let orderLine = new OrderLine();
+//total函数直接调用获取结果
+console.log("price" + orderLine.price + "total" + orderLine.total);
+
+const num = observable.box(10);
+const plus = computed(() => num.get() > 0);
+ autorun(() => {
+     console.log(plus.get());
+ });
+
+ num.set(-19);
+ num.set(-1);
+ num.set(1);
+
+ const price = observable.box(199);
+ const storage = observable.box(15);
+
+ const allPrice = computed(() => price.get() * storage.get());
+ console.log(`计算${allPrice}`);
+
+ configure({enforceActions: "always"});
+ class Store {
+     @observable number = 0;
+     @action add = () => {
+         this.number++;
+     }
+ }
+ const newStore = new Store();
+ newStore.add();
+ console.log(newStore.number);
+
+ //由MyState进行状态管理
+ class MyState {
+     @observable num1 = 0;
+     @observable num2 = 100;
+
+     @action addNum1 = () => {
+         this.num1++;
+     };
+     @action addNum2 = () => {
+         this.num2++;
+     };
+     @computed get total(){
+         return this.num1 + this.num2;
+     }
+ }
+ //实例化的state状态管理作为props传入子组件
+ const newState = new MyState();
+
+ //使用observer修饰无状态组件
+ const AllNum = observer((props) => <div>num1 + num2 = {props.store.total}</div>);
+
+ const Main = inject('persons')(observer((props) => (
+
+     <div>
+         <p>num1 = {props.store.num1}</p>
+         <p>num2 = {props.store.num2}</p>
+         <div>
+             <button onClick={props.store.addNum1}>num1 + 1</button>
+             <button onClick={props.store.addNum2}>num2 + 1</button>
+         </div>
+         <ul>
+             {props.persons.value.length && props.persons.value.map((item, index) => <li key={index}>{item.name}: {item.age}</li>)}
+         </ul>
+     </div>
+ )));
+
+ const persons = observable.box([{
+     name: 'li',
+     age: '24'
+ }, {
+     name: 'sun',
+     age: '26'
+ }]);
+
+ console.log(persons);
+
+ @observer
+ class MyPerson extends React.Component{
+     render() {
+         return (
+             <div>
+                 <Main store={newState}/>
+                 <AllNum store={newState}/>
+             </div>
+         );
+     }
+ }
+
+ @observer
+ class App extends React.Component{
+     render() {
+         return (
+             <Provider persons={persons}>
+                 <MyPerson />
+             </Provider>
+         );
+     }
+ }
+
+
+ ReactDOM.render(
+     <App />,
+     document.getElementById('root')
+ );
