dc3cdf75d271ec4cd522956c55f2bcc3eef26d42
lichang <798602874@qq.com>
2019-09-10 14:12:24 +0800

    更新使用内置hook，useState、useEffect

--- src/component/hook/index.jsx
+++ src/component/hook/index.jsx
@@ -4,10 +4,17 @@
  * @describe: useState Hook 示例
  * @create: 2019-09-10 11:44
  */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function MyHook() {
+    // 变相使用 state : count 和 setCount，并将count初始值设置为 0
     const [count, setCount] = useState(0);
+    const [fruit, setFruit] = useState('banana');
+    const [todo, setTodo] = useState([{text: 'learn hooks'}]);
+
+    useEffect(() => {
+        document.title = `you click ${count} times`
+    });
 
     return (
         <div>
@@ -15,6 +22,12 @@ function MyHook() {
             <button onClick={() => setCount(count + 1)}>
                 Click me
             </button>
+            <br/>
+            <label>爱吃的水果：</label>
+            <input value={fruit} onChange={(event) => setFruit(event.target.value)}/>
+            <br/>
+            <label>待完成事项：</label>
+            <input value={todo[0].text} onChange={(event) => setTodo(event.target.value)}/>
         </div>
     );
 }
